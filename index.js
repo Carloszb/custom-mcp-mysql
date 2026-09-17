db6d33c0981924381ff65ee1263aa24c69e28c16
Carlos Zapico Bello <carlos4zb50@gmail.com>
2026-09-17 15:33:15 +0200

    FIX: dotenv eliminado

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import dotenv from 'dotenv';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-dotenv.config({ path: join(__dirname, '.env') });
+
 
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
